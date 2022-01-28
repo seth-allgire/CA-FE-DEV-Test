35d561003a6b8d77e48de1a65542caf416da2fa4
Seth Allgire <seth.allgire@gmail.com>
2022-01-28 14:43:17 -0600

    adds prev/next buttons to carousel

--- resources/CA-FE-DEV-Test.js
+++ resources/CA-FE-DEV-Test.js
@@ -1,6 +1,10 @@
 var imagePosition = 1;
 showImage(imagePosition);
 
+function progressSlides(n) {
+  showImage((imagePosition += n));
+}
+
 function currentImage(n) {
   showImage((imagePosition = n));
 }
@@ -10,6 +14,12 @@ function showImage(n) {
   var images = document.getElementsByClassName("carousel-image");
   var carouselNavs = document.getElementsByClassName("carousel-nav");
 
+  if (n > images.length) {
+    imagePosition = 1;
+  }
+  if (n < 1) {
+    imagePosition = images.length;
+  }
   for (i = 0; i < images.length; i++) {
     images[i].style.display = "none";
   }
